2b84fba1b542cf6806d35b88089a587bfdc9da04
Dr Cyril Pernet <juh9g@osf.io>
2014-02-21 15:35:25 +0000

    Added via the Open Science Framework

---
+++ spmup_auto_mask.m
@@ -0,0 +1,59 @@
+function M = spmup_auto_mask(V,threshold,fig)
+
+% routine to compute a mask from V, a time-series of memory mapped images
+% this gives a similar (but more inclusive) mask than SPM. Data are
+% smoothed the the average is used as a mask above threshold * by all
+% voxels that are non zeros differences in the time series
+%
+% FORMAT M = spmup_auto_mask(V,threshold)
+%
+% INPUT  V memory mapped images (see spm_vol)
+%        threshold the percentage of signal to keep (default = 0.2)
+%        fig 'on' or 'off' (default) to image the mask and average image
+%
+% OUTPUT M the mask image
+%
+% Cyril Pernet v1 17-Feb-2014
+% ----------------------------
+% Copyright (c) SPMU+ toolbox
+
+threshold = 0.2;
+fig = 'off';
+if nargin == 2
+    threshold = varargin{2};
+elseif nargin == 3
+    threshold = varargin{2};
+    fig = varagin{3};
+end
+
+
+% get the data
+Y = spm_read_vols(V);
+
+% compute the mean of normalized smoothed data
+% by filling small holes we are more inclusive
+for im=1:size(Y,4)
+    Ys(im,:,:,:) = smooth3(squeeze(Y(:,:,:,im))); 
+end
+Ys = (Ys-min(Ys(:)));
+Ys = Ys ./ max(Ys(:)); 
+Avg = squeeze(mean(Ys,1));
+
+% mask is any non 0 difference between successive scan for average values
+% abobve the specified threshold
+M = squeeze(any(diff(Ys))) .* squeeze(Avg>threshold);
+
+if strcmp(fig,'on')
+    figure('Name','Mask')
+    set(gcf,'Color','w')
+    colormap('gray')
+    for im=1:size(M,3)
+        subplot(1,2,1);
+        imagesc(flipud(squeeze(M(:,:,im))'));
+        title(['slice' num2str(im)],'Fontsize',14);
+        axis square; subplot(1,2,2);
+        imagesc(flipud(squeeze(Avg(:,:,im))'));
+        title('Average smoothed image','Fontsize',14);
+        axis square; pause
+    end
+end
