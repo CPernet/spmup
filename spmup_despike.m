e9e26e4fb4b80ee81461a8958cb02d8f69127d4b
Dr Cyril Pernet <juh9g@osf.io>
2014-03-12 14:33:56 +0000

    Updated via the Open Science Framework

--- spmup_despike.m
+++ spmup_despike.m
@@ -1,7 +1,7 @@
-function spmup_despike(varargin)
+function despiked = spmup_despike(varargin)
 % 
-% SPM U+ routine to 'despike' fMRI time-series in a similar way as AFNI does
-% Note is requires the statistics toolbox (nansum, icdf are called)
+% SPM U+ routine to 'despike' fMRI time-series 
+% Note the statistics toolbox if required (nansum, icdf are called)
 %
 % FORMAT spmup_despike
 %        spmup_despike(P)
@@ -10,41 +10,57 @@ function spmup_despike(varargin)
 %        spmup_despike(P,[],flags)
 %
 % INPUT if none the user is prompted
-%       P the names of the fMRI images (time-series)
+%       P the names of the fMRI images (time-series) or the 4D matrix of data
 %       M the name of the mask
 %       flags defines options to be used
-%             'flags.auto_mask','off' or 'on' if M is not provided, auto_mask is
+%             - flags.auto_mask,'off' or 'on' if M is not provided, auto_mask is
 %             'on' but if set to 'off' the user is prompted to select a mask
+%             - flags.method defines the despiking method, by default this is
+%             set to flags.method = 'median' but can be chaged to 'curve',
+%             in that case a smooth curve is fitted to the data folowing
+%             the AFNI implementation
+%             - flags.window defines the number of consecutive images to use 
+%             to despike the data ; for instance flags.method = 'median'
+%             and flags.window = 3 means that each data point is 1st
+%             substituted by a moving 3 points median and the resulting fit
+%             is used to detrermine outliers (see below)
 %
-% OUTPUT spmup_despike_log is saved onto disk where the data are
+% OUTPUT despiked is either the list of despiked images save onto disk or 
+%        the despiked data, depending on the input P
+%
+%        spmup_despike_log is saved onto disk where the data are
 %        spmup_despike_log can be reviewed using spmup_review_despike_log
 %        spmup_despike_log is structure with the fields:
-%        - P the list of files (data) analyzed
+%        - P the list of files (data) analyzed if given or selected
+%        - flags the flags used
 %        - outlying_voxels the proportion of outlying voxels per volume
 %        - outlying_volumes a binary classification of volumes
 %        - despiked_voxels the proportion of despiked voxels per volume
 %        - class a 4d binary matrix indicating despiked voxels
 %
 %        the new dataset with the spikes removed is also written in the
-%        data folder with the prefix 'despiked_'
+%        data folder with the prefix 'despiked_' if no input or P is a series 
+%        of names given as input
 %
 % --------------------------------------------------------------------------
 % First, one look for outlier voxels based on the Median Absolute Deviation
 % Here the MAD is median absolute value of time series minus trend.
-% The trend is optained using son_detrend (2nd order polynomial)
+% The trend is optained using spm_detrend (2nd order polynomial)
 % Outliers are then those voxels with values outside 
-% k = alphav * sqrt(pi/2) * MAD  --- this is the similar as 3dToutcount
+% k = alphav * sqrt(pi/2) * MAD  
+% see http://afni.nimh.nih.gov/pub/dist/doc/program_help/3dToutcount.html
 %
-% Second, if some volume outliers are found, despike
+% Second, if some volume outliers are found, data are despiked
 % Note that the despiking is not based on the results of the above, some 
 % voxels from volume not seen as outliers can still be despiked.
-% The data are 1st smoothed either with a median filter (N/30 points window)
-% or using a smooth curve (as in AFNI - see flag options) and then we look
+% The data are 1st smoothed either with a median filter (using the window 
+% parameter- see flag options) or using a smooth curve and then we look
 % for outliers in the residuals and the data are interpolated as in AFNI.
-% Finaly, we write the data with the prefix despiked_  
+% see http://afni.nimh.nih.gov/pub/dist/doc/program_help/3dDespike.html
 % --------------------------------------------------------------------------
 %
 % Cyril Pernet 17 Feb 2014
+% revision for windowing, and 4D input/output 12 MArch 2014 
 % --------------------------------------------------------------------------
 % Copyright (c) SPM U+ toolbox
 
@@ -87,6 +103,14 @@ elseif nargin == 3
             error('oops curve fitting is not implemented yet')
          end
     end
+    
+    if isfield(varargin{3},'window')
+        flags.window = varargin(3).window;
+        if ~isnumeric(flags.window)
+            error('the window parameter must be a real number')
+        end
+    end
+    
 end
 
 
@@ -94,10 +118,28 @@ end
 % memory mapped data
 if get_data == 1;
     [P,sts] = spm_select(Inf,'image','select the time seires',[],pwd,'.*',1);
+    V = spm_vol(P);
 else
     P = varargin{1};
+    if ischar(P)
+        V = spm_vol(P);
+        % use the spm_read_vols routine but bypass orientation check
+        N = numel(V);
+        Y = zeros([V(1).dim(1:3),N]);
+        for i=1:N
+            for p=1:V(1).dim(3)
+                Y(:,:,p,i) = spm_slice_vol(V(i),spm_matrix([0 0 p]),V(i).dim(1:2),0);
+            end
+        end
+    else
+        if numel(size(P)) == 4
+            Y = P; N = size(Y,4);
+        else
+            error('input data are not char nor 4D data matrix, please check inputs')
+        end
+    end
 end
-V = spm_vol(P);
+
 
 % memory mapped mask
 if get_mask == 1;
@@ -115,7 +157,7 @@ elseif get_mask == 0 && strcmp(flags.auto_mask,'off')
     end
 else
     disp('generating a mask')
-    Mask = spmup_auto_mask(V);
+    Mask = spmup_auto_mask(Y);
 end
 
 % figure('Name','Mask')
@@ -126,9 +168,6 @@ end
 %     pause
 % end
 
-% data
-Y = spm_read_vols(V);
-N = size(Y,4);
 
 %% this part is similar as 3dToutcount 
 % http://afni.nimh.nih.gov/pub/dist/doc/program_help/3dToutcount.html
@@ -165,7 +204,7 @@ MADN = repmat((MAD./.6745),1,N); % this is almost like 3.5*MAD but better
 outlying_volumes = (abs(outlying_voxels-M) ./ MADN) > sqrt(chi2inv(0.975,1));
 
 %% now do the despiking
-% this part is similar to 3dDespike execpt we use a simple median smoother
+% this part is similar to 3dDespike execpt we can use a simple median smoother
 % http://afni.nimh.nih.gov/pub/dist/doc/program_help/3dDespike.html
 
 YY =Y;
@@ -184,19 +223,22 @@ if sum(outlying_volumes) ~=0
                     data = squeeze(Y(x,y,z,:))';
                     newdata = nan(size(data));
                     
-                    if mod(ceil(N/30),2) % % as in AFNI /30
-                        window = ceil(N/30);
-                    else
-                        window = floor(N/30);
-                    end
-                    
-                    if window < 3 % need at least 3 points
-                        window = 3;
-                    end
-                    
                     % median smoothing
                     % ------------------
                     if strcmp(flags.method,'median')
+                        
+                        % define window size
+                        if isfield(flags,'window')
+                            window = flags.window;
+                            if window < 3 % need at least 3 points
+                                window = 3;
+                            end
+                        else
+                            window = 3;
+                        end
+                        
+                        % smooth data
+                        % -----------
                         % beginning
                         for p=1:floor(window/2)
                             newdata(p) = median([repmat(data(1),1,ceil(window/2)-p) data(1:p) data(p+1:p+floor(window/2))]);
@@ -213,15 +255,34 @@ if sum(outlying_volumes) ~=0
                         end
                         newdata(N) = data(N);
                         
-                    % N/30 points curve fitting (1st order)
-                    % ------------------------------------
+                    % curve fitting (1st order)
+                    % --------------------------
                     elseif strcmp(flags.method,'curve')
+                      
+                        % define window size
+                        if isfield(flags,'window')
+                            window = flags.window;
+                        else
+                            if mod(ceil(N/30),2) % % as in AFNI /30
+                                window = ceil(N/30);
+                            else
+                                window = floor(N/30);
+                            end
+                        end
                         
+                        if window < 3 % need at least 3 points
+                            window = 3;
+                        end
+                        
+                        % fit data
+                        % -----------
                         % f(t) = a+b*t+c*t^2+cumsum(d*sin((2*pi*t)/T)+e*cos((2*pi*t)/T))
                         % T duration of the time series
                         % a,b,c,d,e are minimized for v(t)-f(t)
-                        % TO DO - use fminsearch on that function
-                        
+                        % TO DO -- iterate per window 
+                        % Mini = @(x,t) v - (x(1)+x(2)*t+x(3)*t^2+cumsum(x(4)*sin((2*pi*t)/T)+e*cos((2*pi*t)/T)));
+                        % X = fminsearch(@(x) Mini(x,t),X0)
+                         
                     end
                     
                     
@@ -279,18 +340,26 @@ else
     drawnow; saveas(gcf, 'despiking - volume outlier detection.eps','psc2'); close(gcf)
 end
 
-%% write the data
-for v=1:size(Y,4)
-    disp('writing data')
-    V(v).descrip = 'spmu+ despiked';
-    [pathstr,name,ext]= fileparts(V(v).fname);
-    V(v).fname = [pathstr filesep 'despiked_' name ext];
-    spm_write_vol(V(v),squeeze(YY(:,:,:,v)));
+%% write and return the data
+if ischar(P)
+    for v=1:size(Y,4)
+        disp('writing data')
+        V(v).descrip = 'spmu+ despiked';
+        [pathstr,name,ext]= fileparts(V(v).fname);
+        V(v).fname = [pathstr filesep 'despiked_' name ext];
+        despiked{v} = V(v).fname;
+        spm_write_vol(V(v),squeeze(YY(:,:,:,v)));
+    end
+else
+    despiked = YY;
 end
 
 %% write the report
 disp('saving spmup_despike_log')
-spmup_despike_log.P = P;
+if ischar(P)
+    spmup_despike_log.P = P;
+end
+spmup_despike_log.flags = flags;
 spmup_despike_log.outlying_voxels = outlying_voxels;
 spmup_despike_log.outlying_volumes = outlying_volumes;
 spmup_despike_log.despiked_voxels = despiked_voxels;
