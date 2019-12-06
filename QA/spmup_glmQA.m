577968bfef96fdb7a4ae32a8c253df842eb6022d
cyril.pernet@ed.ac.uk <wamcyril@gmail.com>
2019-12-06 08:54:14 +0000

    todo

---
+++ QA/spmup_glmQA.m
@@ -0,0 +1,22 @@
+function GMLQA = spmup_glmQA(SPM,options)
+
+% compute QA metrics related to glm 
+%
+% FORMAT: GMLQA = spmup(SPM,options)
+%         options are 'write_R2' to write the R2 value per voxel as an image
+%
+% INPUT: SPM is the SPM.mat 
+%        note outputs vary depending wether the model was estimated or not
+%
+% OUPUTS: GLMQA.design reports the xxx
+%         GLMQA.B2N is the BOLD to noise ratio 
+%         GLMQA.R2 the average of model fit of the data
+
+%% inputs
+
+
+%% compute
+
+
+%% write options
+
