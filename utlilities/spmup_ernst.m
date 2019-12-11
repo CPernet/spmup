9fe8027db584389584a19205a44422118a0689df
cyril.pernet@ed.ac.uk <wamcyril@gmail.com>
2019-12-11 12:53:07 +0000

    T1time as option

--- utlilities/spmup_ernst.m
+++ utlilities/spmup_ernst.m
@@ -1,13 +1,22 @@
-function flip_angle = spmup_ernst(TR)
+function flip_angle = spmup_ernst(TR,ToneTime)
 
-% assuming T1 ~ 1400ms for gray matter, this returns the optimal flip
-% angle for a given TR: cos(flip_angle) = exp(-TR/T1)
+% this returns the theoretical optimal flip angle for a given TR:
+% cos(flip_angle) = exp(-TR/T1)
 %
+% FORMAT flip_angle = spmup_ernst(TR,ToneTime)
+%
+% INPUT TR of the pulse sequence
+%       ToneTime (optional) is the T1 recovery time, default is 1400 optimized for gray matter
+
 % Cyril Pernet August 2017
 % --------------------------------------------------------------------------
 % Copyright (c) SPM Utility Plus toolbox
 
-flip_angle = acosd(exp(-TR/1400));
+if nargin == 1
+  ToneTime = 1400;
+end
+
+flip_angle = acosd(exp(-TR/ToneTime));
 
 
 
