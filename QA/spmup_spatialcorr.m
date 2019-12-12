243a3657e6b2f425498f8751bf2675681c922f20
cyril.pernet@ed.ac.uk <wamcyril@gmail.com>
2019-12-12 06:59:59 +0000

    takes figure argument in

--- QA/spmup_spatialcorr.m
+++ QA/spmup_spatialcorr.m
@@ -4,10 +4,11 @@ function [volume_outliers, slice_outliers] = spmup_spatialcorr(varargin)
 % this allows testing that images are alike and spots drop out easily
 % also create the mean image, sd, and binary mask 
 %
-% FORMAT: [volume_outliers, slice_outliers] = fMRI_info
-% FORMAT: [volume_outliers, slice_outliers] = fMRI_info(P)
+% FORMAT: [volume_outliers, slice_outliers] = spmup_spatialcorr
+% FORMAT: [volume_outliers, slice_outliers] = spmup_spatialcorr(P,'figure',value)
 %
-% INPUT:  P are the fmRI data
+% INPUT:  P are the fMRI data
+%         'figure' is set as 'on' 'off' or 'save' (default)
 %
 % OUTPUT: volume_outliers indexes volumes with an average correlation between
 %                         slices bigger or lower than the other volumes
@@ -72,7 +73,15 @@ else
         end
     end
 end
-    
+   
+fig = 'save';
+if nargin > 1 && strcmpi(varargin{2},'figure')
+    fig = 'on';
+    if nargin == 3
+        fig = varargin{3};
+    end
+end
+
 %% correlations between slices within each volume
 % -----------------------------------------------
 
@@ -82,11 +91,12 @@ for j=size(Y,4):-1:1
     end
 end
 
-figure('Name','Spatial QA')
-set(gcf,'Color','w','InvertHardCopy','off', 'units','normalized','outerposition',[0 0 1 1])
-
-subplot(2,2,1); imagesc(r); ylabel('slices'); xlabel('volume nb')
-title('Correlations between slices within each volume')
+if strcmpi(fig,'on') || strcmpi(fig,'save')
+    figure('Name','Spatial QA')
+    set(gcf,'Color','w','InvertHardCopy','off', 'units','normalized','outerposition',[0 0 1 1])
+    subplot(2,2,1); imagesc(r); ylabel('slices'); xlabel('volume nb')
+    title('Correlations between slices within each volume')
+end
 
 r = nanmean(r,1); % average over slices
 volume_outliers = spmup_comp_robust_outliers(r);
@@ -96,9 +106,11 @@ else
     disp('no volume outliers detected');
 end
 
-subplot(2,2,3); plot(r,'LineWidth',2); ylabel('correlation value'); xlabel('volume nb')
-hold on; tmp = volume_outliers.*r; tmp(tmp==0)=NaN; plot(tmp,'ro','LineWidth',3);
-grid on; axis tight; hold on; title('Average correlations per volume'); clear tmp
+if strcmpi(fig,'on') || strcmpi(fig,'save')
+    subplot(2,2,3); plot(r,'LineWidth',2); ylabel('correlation value'); xlabel('volume nb')
+    hold on; tmp = volume_outliers.*r; tmp(tmp==0)=NaN; plot(tmp,'ro','LineWidth',3);
+    grid on; axis tight; hold on; title('Average correlations per volume'); clear tmp
+end
 
 %% correlations between volumes within each slice
 % -----------------------------------------------
@@ -109,9 +121,11 @@ for j=(size(Y,4)-1):-1:1
     end
 end
 
-subplot(2,2,2); imagesc(r)
-title('Correlations between volumes within each slice')
-ylabel('slices'); xlabel('volume nb')
+if strcmpi(fig,'on') || strcmpi(fig,'save')
+    subplot(2,2,2); imagesc(r)
+    title('Correlations between volumes within each slice')
+    ylabel('slices'); xlabel('volume nb')
+end
 
 r = nanmean(r,2); % average over volumes
 slice_outliers = spmup_comp_robust_outliers(r);
@@ -121,9 +135,20 @@ else
     disp('no slice outliers detected');
 end
 
-subplot(2,2,4); plot(r,'LIneWidth',2); ylabel('correlation value'); xlabel('slice nb')
-hold on; tmp = slice_outliers.*r; tmp(tmp==0)=NaN; plot(tmp,'ro','LineWidth',3);
-grid on; axis tight; hold on; title('Average correlations per slice'); clear tmp
+if strcmpi(fig,'on') || strcmpi(fig,'save')
+    subplot(2,2,4); plot(r,'LIneWidth',2); ylabel('correlation value'); xlabel('slice nb')
+    hold on; tmp = slice_outliers.*r; tmp(tmp==0)=NaN; plot(tmp,'ro','LineWidth',3);
+    grid on; axis tight; hold on; title('Average correlations per slice'); clear tmp
+end
 
+if strcmpi(fig,'save')
+    [filepath,filename] = fileparts(V(1).fname);
+    try
+        print (gcf,'-dpdf', '-bestfit', fullfile(filepath,[filename '_spatial_correlation.pdf']));
+    catch
+        print (gcf,'-dpdf', fullfile(filepath,[filename '_spatial_correlation.pdf']));
+    end
+    close(gcf)
+end
 
 
