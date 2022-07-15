a41c7752997b99ad3c5a2e66100b66e813e96449
CPernet <wamcyril@gmail.com>
2022-07-15 17:22:50 +0200

    returns subject cell array updated

--- bids/run_spmup_bids.m
+++ bids/run_spmup_bids.m
@@ -1,4 +1,4 @@
-function run_spmup_bids(BIDS,subjects, varargin)
+function subjects = run_spmup_bids(subjects, varargin)
 
 % SPMUP can be run using SPM batch mode but you can also analyze an entire
 % fMRI BIDS dataset automatically using this function - it will unpack zip
@@ -12,14 +12,16 @@ function run_spmup_bids(BIDS,subjects, varargin)
 %
 % FORMAT run_spmup_bids(BIDS_dir, options)
 %
-% INPUT BIDS_dir is the BIDS directory to analyse
-%       options are a set of key/value pairs updating the defaults
-%               see spmup_getoptions
+% INPUT subjects is the structure coming out of spmup_BIDS_unpack
+%       options is either a set of key/value pairs matching fields from 
+%               spmup_getoptions or direction the options structure
 %
-% OUTPUT the preprocessed and analzed data are written on disk 
+% OUTPUT the preprocessed and analyzed data are written on disk 
 %
-% Example usage [BIDS,subjects] = spmup_BIDS_unpack(BIDS_dir,options);
-%               run_spmup_bids(BIDS,subjects, 'GLM', 'off')
+% Example usage BIDS_dir        = 'F:\WakemanHenson_Faces\fmri';
+%               options         = spmup_getoptions(BIDS_dir); 
+%               [BIDS,subjects] = spmup_BIDS_unpack(BIDS_dir,options);
+%               run_spmup_bids(subjects)
 %
 % Cyril Pernet July 2022
 % --------------------------
@@ -28,47 +30,42 @@ function run_spmup_bids(BIDS,subjects, varargin)
 %% Set options, matlab path and get data
 
 % check data are there
-for s = 1:length(subjects)
-    for f = 1:size(subjects{s}.anat,1)
-        if ~exist(subjects{s}.anat{f},'file')
-            error(' %s file is missing',subjects{s}.anat{f})
-        end
-    end
-    
-    for f = 1:size(subjects{s}.func,1)
-        if ~exist(subjects{s}.func{f},'file')
-            error(' %s file is missing',subjects{s}.func{f})
-        end
-    end
-    
-    if isfield(subjects{s},'fmap')
-        for f = 1:size(subjects{s}.fmap,1)
-            if ~exist(subjects{s}.fmap{f},'file')
-            error(' %s file is missing',subjects{s}.fmap{f})
-            end
-        end
-    end
-end
+% for s = 1:length(subjects)
+%     for f = 1:size(subjects{s}.anat,1)
+%         if ~exist(subjects{s}.anat{f},'file')
+%             error(' %s file is missing',subjects{s}.anat{f})
+%         end
+%     end
+%     
+%     for f = 1:size(subjects{s}.func,1)
+%         if ~exist(subjects{s}.func{f},'file')
+%             error(' %s file is missing',subjects{s}.func{f})
+%         end
+%     end
+%     
+%     if isfield(subjects{s},'fmap')
+%         for f = 1:size(subjects{s}.fmap,1)
+%             if ~exist(subjects{s}.fmap{f},'file')
+%             error(' %s file is missing',subjects{s}.fmap{f})
+%             end
+%         end
+%     end
+% end
 
 % make sure all folders are in the path 
 addpath(genpath(fullfile(fileparts(which('spm.m')),['toolbox' filesep 'spmup'])));
 
-% set default options
-options = spmup_getoptions(BIDS_dir);
-
 % check inputs
-if nargin > 2
-    for v=1:2:length(varargin)
-        if any(contains(varargin{v},fieldnames(options)))
-            options.(varargin{v}) = varargin{v+1};
+if nargin >= 2
+    if nargin == 2 && isstruct(varargin{1})
+        options = varargin{1};
+    else
+        for v=1:2:length(varargin)
+            if any(contains(varargin{v},fieldnames(options)))
+                options.(varargin{v}) = varargin{v+1};
+            end
         end
     end
-end
-
-if strcmpi(options.norm,'T1norm') && isempty(options.VDM)
-    warning(' Calhoun et al. (2017) showed T1 normalization is not good without FieldMaps \n (%s) add VDM map to options \n swithing to ''EPInorm''', ...
-        'https://onlinelibrary.wiley.com/doi/full/10.1002/hbm.23737')
-    options.norm = 'EPI';
 end
 
 %% preprocess
@@ -78,19 +75,11 @@ catch no_parpool
     disp(no_parpool.message)
 end
 
-anatQA_grp = cell(numel(subjects),1);
-fMRIQA_grp = cell(numel(subjects),1);
-updated_subjects = cell(numel(subjects),1);
-parfor s =1:numel(subjects)
-    [anatQA_grp{s}, fMRIQA_grp{s}, updated_subjects{s}] = ...
-        spmup_BIDS_preprocess(BIDS, subjects, s, options);
+parfor s =1: numel(subjects)
+    subject{s} = spmup_BIDS_preprocess(subjects{s}, options)
+    if strcmpi(options.GLM,'on')
+        subject{s} = spmup_BIDS_1rstlevel(subjects{s}, options)
+    end
 end
-subjects = updated_subjects;
-clear updated_subjects
-
-
-%% run first level GLM
-% for s=1:numel(subjects)
-%     [] = spmup_BIDS_1rstlevel(BIDS_dir, BIDS, subjects, s, options)
-% end
+save subjects subjects
 
