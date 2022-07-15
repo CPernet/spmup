9ff8742b8f86dfb2fa3677b3f2ebe88c786d4a17
CPernet <wamcyril@gmail.com>
2022-07-15 17:18:43 +0200

    returns FD, globals + general tidy up

--- QA/spmup_realign_qa.m
+++ QA/spmup_realign_qa.m
@@ -1,4 +1,4 @@
-function new_files = spmup_realign_qa(P,varargin)
+function [new_files,FD,glo] = spmup_realign_qa(P,varargin)
 
 % implement different quality control 'tools' for
 % fMRI realigned data using SPM
@@ -18,7 +18,7 @@ function new_files = spmup_realign_qa(P,varargin)
 %       'Framewise displacement': 'on' (default) or 'off'
 %              --> plots displacement (FD and RMS)                   
 %              --> generates/appends new design.txt file with displacement outliers
-%       'Voltera': 'off' (fefault) or 'on'
+%       'Voltera': 'off' (default) or 'on'
 %              --> clompute the motion parameter expansion
 %              --> generates/appends new design.txt file with displacement outliers
 %       'Globals': 'on' (default) or 'off'
@@ -37,51 +37,7 @@ function new_files = spmup_realign_qa(P,varargin)
 
 %% validate inputs
 spm('Defaults','fmri')
-current = pwd;
- 
-% check P
-if nargin == 0
-    [P,sts] = spm_select(Inf,'image' ,'Select your fMRI time series',{},pwd,'.*',Inf);
-    if sts == 0
-        return
-    end
-    V = spm_vol(P);
-    % bypass orientation check allowing to look at raw data
-    N = numel(V);
-    Y = zeros([V(1).dim(1:3),N]);
-    for i=1:N
-        for p=1:V(1).dim(3)
-            Y(:,:,p,i) = spm_slice_vol(V(i),spm_matrix([0 0 p]),V(i).dim(1:2),0);
-        end
-    end
-else
-    if ischar(P)
-        V = spm_vol(P);
-        N = numel(V);
-        Y = zeros([V(1).dim(1:3),N]);
-        for i=1:N
-            for p=1:V(1).dim(3)
-                Y(:,:,p,i) = spm_slice_vol(V(i),spm_matrix([0 0 p]),V(i).dim(1:2),0);
-            end
-        end
-    elseif iscell(P)
-        for v=size(P,1):-1:1
-            V(v) =spm_vol(P{v});
-        end
-        
-        for i=numel(V):-1:1
-            for p=V(1).dim(3):-1:1
-                Y(:,:,p,i) = spm_slice_vol(V(i),spm_matrix([0 0 p]),V(i).dim(1:2),0);
-            end
-        end
-    else
-        if numel(size(P)) == 4 % this is already data in
-            Y = P; 
-        else
-            error('input data are not char nor 4D data matrix, please check inputs')
-        end
-    end
-end
+opts.indent = ' '; % for spm_jsonwrite
 
 % check options
 MotionParameters      = 'on';
@@ -96,117 +52,154 @@ fig                   = 'save';
 if nargin >1
    for v=1:(nargin-1)
       if strcmpi(varargin{v},'Motion Parameters')
-           MotionParameters = varargin{v+1};
+           MotionParameters      = varargin{v+1};
       elseif strcmpi(varargin{v},'Radius')
-           Radius = varargin{v+1};
+           Radius                = varargin{v+1};
       elseif strcmpi(varargin{v},'Framewise Displacement')
            FramewiseDisplacement = varargin{v+1};
       elseif strcmpi(varargin{v},'Voltera')
-           Voltera = varargin{v+1};
+           Voltera               = varargin{v+1};
       elseif strcmpi(varargin{v},'Globals')
-           Globals = varargin{v+1};
+           Globals               = varargin{v+1};
       elseif strcmpi(varargin{v},'Movie')
-           Movie = varargin{v+1};
+           Movie                 = varargin{v+1};
       elseif strcmpi(varargin{v},'Coordinate')
-           Coordinates = varargin{v+1};
+           Coordinates           = varargin{v+1};
       elseif strcmpi(varargin{v},'Figure')
-           fig = varargin{v+1};
+           fig                   = varargin{v+1};
       end
    end
+end
+
+% check P
+if nargin == 0
+    [P,sts] = spm_select(Inf,'image' ,'Select your fMRI time series',{},pwd,'.*',Inf);
+    if sts == 0
+        return
+    end
+    V = spm_vol(P);
+    if strcmp(Movie,'on')
+        % bypass orientation check allowing to look at raw data
+        N = numel(V);
+        Y = zeros([V(1).dim(1:3),N]);
+        for i=1:N
+            for p=1:V(1).dim(3)
+                Y(:,:,p,i) = spm_slice_vol(V(i),spm_matrix([0 0 p]),V(i).dim(1:2),0);
+            end
+        end
+    end
+else
+    if ischar(P)
+        V = spm_vol(P);
+        if strcmp(Movie,'on')
+            N = numel(V);
+            Y = zeros([V(1).dim(1:3),N]);
+            for i=1:N
+                for p=1:V(1).dim(3)
+                    Y(:,:,p,i) = spm_slice_vol(V(i),spm_matrix([0 0 p]),V(i).dim(1:2),0);
+                end
+            end
+        end
+    elseif iscell(P)
+        for v=size(P,1):-1:1
+            V(v) =spm_vol(P{v});
+        end
+        if strcmp(Movie,'on')
+            for i=numel(V):-1:1
+                for p=V(1).dim(3):-1:1
+                    Y(:,:,p,i) = spm_slice_vol(V(i),spm_matrix([0 0 p]),V(i).dim(1:2),0);
+                end
+            end
+        end
+    else
+        if numel(size(P)) == 4 % this is already data in
+            Y = P;
+            MotionParameters = 'off'; FramewiseDisplacement = 'off';
+            Voltera = 'off'; Globals = 'on'; Movie = 'on';
+            fig = 'save';
+        else
+            error('input data are not char nor 4D data matrix, please check inputs')
+        end
+    end
 end
 
 % if movie we need AC
 if strcmp(Movie,'on')
     if isempty(Coordinates)
-        Coordinates = V(1).dim/2; % default
-%         Coordinate = input('Coordinate to centre the movie on is missing [x y z]:','s');
-%         if ~contains(Coordinate,'[') && ~contains(Coordinate,']')
-%             Coordinate = ['[' Coordinate ']'];
-%         end
-%         Coordinate = eval(Coordinate);
+        if exist('V','var')
+            Coordinates = round(V(1).dim/2); % default
+        else
+            Coordinates = round(size(Y)/2);
+            Coordinates = Coordinates(1:3);
+        end
     end
 end
 
-findex = 1; % index for the new_files variables
+%% look at motion parameters
 
-[filepath,filename]=fileparts(V(1).fname);
-motion_file = dir(fullfile(filepath,['rp*' filename(round(length(filename)/2):end) '.txt']));
-if size(motion_file,1) > 1
-  error('more than one rp*.txt file was found that partially matches the time series name')
+findex              = 1; % index for the new_files variables
+[filepath,filename] = fileparts(V(1).fname);
+motion_file         = dir(fullfile(filepath,'rp*.txt'));
+if size(motion_file,1)>1
+    motion_file = motion_file(arrayfun(@(x) contains(x.name,filename), motion_file)).name;
+    motion_file = fullfile(filepath,motion_file);
+else
+    motion_file = fullfile(filepath,motion_file.name);
 end
 
-%% look at motion parameters
 if strcmpi(FramewiseDisplacement,'on')
-  % FD is computed at the motion stage
-  MotionParameters = 'on'; 
+    MotionParameters = 'on';
 end
 
 if strcmpi(MotionParameters,'on')
-    disp('getting displacement ... ')
-
-    [FD,RMS] = spmup_FD(fullfile(motion_file.folder, motion_file.name),...
-                        'Radius', Radius, ...
-                        'Figure', fig);
+    FD = spmup_FD(motion_file, 'Radius', Radius, 'Figure', fig);
+else
+    FD = [];
 end
 
 %% look at globals
 
-if strcmpi(Globals,'on')
-  
-    disp('computing globals for outliers ... ')
-    
+if strcmpi(Globals,'on')   
     glo = zeros(length(V),1);
     for s=1:length(V)
         glo(s) = spm_global(V(s));
     end
-    glo = spm_detrend(glo,1); % since in spm the data are detrended
+    glo        = spm_detrend(glo,1); % since in spm the data are detrended
     g_outliers = spmup_comp_robust_outliers(glo, 'Carling');
         
     % figure
-    figure('Name', 'Globals outlier detection', ...
-           'Visible', 'on');
-    
-    if ismember(fig, {'off', 'save'})
-          set(gcf,'Visible','off')
-    end
-
-    set(gcf,'Color','w', ...
-            'InvertHardCopy','off', ...
-            'units','normalized', ...
-            'outerposition',[0 0 1 1])
-          
-    hold on;       
-    
-    plot(glo,'LineWidth',3); 
-
-    tmp = g_outliers.*glo; 
-    tmp(tmp==0)=NaN; 
-    
-    plot(tmp,'or','LineWidth',3);
-    
-    grid on; 
-    axis tight; 
-    
-    xlabel('scans');
-    ylabel('mean intensity');
-    title('Global intensity');
-    
-    if strcmpi(fig,'save')
-        output_file = fullfile(filepath, 'spm.ps');
-        if exist(fullfile(filepath,'spm.ps'),'file')
-            output_file = fullfile(filepath, 'spmup_QC.ps');
+    if ~strcmpi(fig,'off')
+        figure('Name','Globals outlier detection')
+        if strcmpi(fig,'on')
+            set(gcf,'Color','w','InvertHardCopy','off', 'units','normalized','outerposition',[0 0 1 1])
+        else
+            set(gcf,'Color','w','InvertHardCopy','off', 'units','normalized','outerposition',[0 0 1 1],'visible','off')
         end
-        print (gcf,'-dpsc2', '-bestfit', '-append', output_file);
-        close(gcf)
-        cd(current)
+        plot(glo,'LineWidth',3); hold on;
+        tmp = g_outliers.*glo; tmp(tmp==0)=NaN;
+        plot(tmp,'or','LineWidth',3); grid on;
+        axis tight; xlabel('scans');
+        ylabel('mean intensity');
+        title('Global intensity');
+        
+        if strcmpi(fig,'save')
+            if exist(fullfile(filepath,'spmup_QC.ps'),'file')
+                print (gcf,'-dpsc2', '-bestfit', '-append', fullfile(filepath,'spmup_QC.ps'));
+            else
+                print (gcf,'-dpsc2', '-bestfit', fullfile(filepath,'spmup_QC.ps'));
+            end
+            close(gcf)
+        end
     end
+else
+    glo = [];
 end
 
 %% make the design.txt file
 
 data = [];
 if strcmpi(FramewiseDisplacement,'on')
-    data = [data FD RMS];
+    data = [data FD];
 end
 
 if strcmpi(Globals,'on')
@@ -216,17 +209,10 @@ end
 if isempty(data) && strcmpi(Voltera,'off')
     new_files = {};
     disp('no design computed, no extra regressors selected')
-    
 else
-    spmup_censoring(fullfile(motion_file.folder, motion_file.name),...
-                    data, ...
-                    'Voltera', Voltera);
-    % remane using P
-    if ~exist(fullfile(motion_file.folder,[filename '_design.txt']), 'file') 
-      movefile(fullfile(motion_file.folder,[motion_file.name(4:end-4) '_design.txt']),...
-               fullfile(motion_file.folder,[filename '_design.txt']))
-    end
-    new_files{findex} = fullfile(motion_file.folder,[filename '_design.txt']);
+    design = spmup_censoring(motion_file, data, 'Voltera', Voltera);
+    save(fullfile(filepath,[filename '_design.txt']), 'design','-ascii')
+    new_files{findex} = fullfile(filepath,[filename '_design.txt']);
     
     % save info about column headers
     options = struct('Voltera', Voltera, ...
@@ -238,25 +224,17 @@ else
     nb_censoring_regressors = size(all_regressors, 2) - numel(metadata.Columns);
     
     for i = 1:numel(1:nb_censoring_regressors)
-      metadata.Columns{end+1} = sprintf('outlier_%04.0f', i);
+        metadata.Columns{end+1} = sprintf('outlier_%04.0f', i);
     end
-    
-    spm_save(spm_file(new_files{findex}, 'ext', '.json'), metadata)
-    
+    spm_jsonwrite(spm_file(new_files{findex}, 'ext', '.json'), metadata, opts)
     findex = findex +1;
-    
 end
-
 
 %% make movies
 if strcmpi(Movie, 'on')
-    new_files{findex} = spmup_movie(Y, ...
-                                    'coordinates', Coordinates,...
-                                    'filename', fullfile(filepath,filename), ...
-                                    'showfig', 'off');
+    new_files{findex} = spmup_movie(Y, 'coordinates', Coordinates,...
+        'filename', fullfile(filepath,filename), 'showfig', 'off');
 end
-cd(current)
-disp('realign QA done');
 
 end
 
@@ -299,7 +277,6 @@ function headers = column_headers(options)
   
   if strcmp(options.FramewiseDisplacement, 'on')
     headers{end + 1, 1} = 'framewise_displacement';
-    headers{end + 1, 1} = 'root_mean_square';
   end
   
   if strcmp(options.Globals, 'on')
