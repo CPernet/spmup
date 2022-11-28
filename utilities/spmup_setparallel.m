851f799ae54a12ad1d26ce8e6e824d2848a348c0
Remi Gau <remi_gau@hotmail.com>
2022-11-28 11:43:26 +0100

    update how parallel toolbox is detected

--- utilities/spmup_setparallel.m
+++ utilities/spmup_setparallel.m
@@ -22,7 +22,7 @@ if evalin( 'base', 'exist(''options'',''var'') == 1' )
 end
 
 addons = ver;
-if any(any(contains(struct2cell(addons), 'Parallel Computing Toolbox')))
+if any(ismember('Parallel Computing Toolbox', {addons.Name}))
     p = gcp('nocreate');
     if isempty(p) % i.e. the parallel toolbox is not already on
         if isempty(N)
