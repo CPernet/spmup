80dbc0f125af9f8daa871524aa693e2eee7980ca
CPernet <wamcyril@gmail.com>
2023-01-06 11:24:58 +0100

    check local dir for download and catch download error

--- system_test/facerep.m
+++ system_test/facerep.m
@@ -1,6 +1,7 @@
 % test bids processing pipeline on SPM's face repetition single subject dataset
 
 %% update path
+local_test_dir = '/indirect/data1/cpernet/';
 
 % in case spmup was not added to the path
 location = fullfile(pwd, '..');
@@ -26,17 +27,45 @@ else
 end
 
 % needed to get function to bidsify the dataset
+if ~exist(fullfile(bids_matlab_path, 'demos', 'spm', 'facerep', 'code'),'dir')
+    command = ['cd ' bids_matlab_path ' && git switch dev'];
+    system(command)
+end        
 addpath(fullfile(bids_matlab_path, 'demos', 'spm', 'facerep', 'code'));
 
-output_dir = bids.util.download_ds('source', 'spm', ...
-                                   'demo', 'facerep', ...
-                                   'out_path', fullfile(pwd, 'facerep', 'source'), ...
-                                   'force', false, ...
-                                   'verbose', true, ...
-                                   'delete_previous', false);
+try
+    if isempty(local_test_dir)
+        output_dir = bids.util.download_ds('source', 'spm', ...
+            'demo', 'facerep', ...
+            'out_path', fullfile(pwd, 'facerep', 'source'), ...
+            'force', false, ...
+            'verbose', true, ...
+            'delete_previous', true);
+    else
+        output_dir = bids.util.download_ds('source', 'spm', ...
+            'demo', 'facerep', ...
+            'out_path', local_test_dir, ...
+            'force', false, ...
+            'verbose', true, ...
+            'delete_previous', true);
+    end
+    
+    BIDS_dir = convert_facerep_ds(fullfile(pwd, 'facerep', 'source'), ...
+        fullfile(pwd, 'facerep', 'raw'));
+    
+catch downloaderr
+    if ~isempty(local_test_dir)
+        if exist(fullfile(local_test_dir,'face_rep'),'dir')
+            output_dir = fullfile(local_test_dir,'face_rep');
+            warning('\ncould not download the facerep data automatically, %s %s',downloaderr.identifier,downloaderr.message)
+            warning('using found directory %s',output_dir)
+            BIDS_dir = convert_facerep_ds(output_dir, fullfile(output_dir, 'raw'));
+        else
+            error('\ncould not download the facerep data automatically, %s %s',downloaderr.identifier,downloaderr.message)
+        end
+    end
+end
 
-BIDS_dir = convert_facerep_ds(fullfile(pwd, 'facerep', 'source'), ...
-                              fullfile(pwd, 'facerep', 'raw'));
 
 %%
 BIDS_dir = fullfile(pwd, 'facerep', 'raw');
