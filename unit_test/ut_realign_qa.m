c87c4872608bcdf5ee63e07d37c8d2f7a96b3386
Remi Gau <remi_gau@hotmail.com>
2023-01-19 08:36:35 +0100

    use matlab unit test framework

--- unit_test/ut_realign_qa.m
+++ unit_test/ut_realign_qa.m
@@ -1,91 +1,98 @@
-%% unit testing for spmup_realign_qa
+function tests = ut_realign_qa
+tests = functiontests(localfunctions);
+end
 
-close all;
-
-%% get data
+function value = input_file()
 test_data_folder = fullfile(test_folder(), 'data');
+value = spm_select('FPlistRec', test_data_folder, '^sub.*bold.nii');
+end
 
-time_series = spm_select('FPlistRec', test_data_folder, '^sub.*bold.nii');
+function value = plot_figures()
+    value = 'off';
+end
 
-plot_figures = 'off';
+function test_smoke(testCase)
 
-%%
-new_files = spmup_realign_qa(time_series, ...
+new_files = spmup_realign_qa(input_file(), ...
                              'Motion Parameters', 'off', ...
                              'Framewise displacement', 'off', ...
                              'Globals', 'off', ...
                              'Voltera', 'off', ...
                              'Movie', 'off', ...
-                             'figure', plot_figures);
+                             'figure', plot_figures());
 
 assert(isempty(new_files));
+end
 
-%% Motion
-new_files = spmup_realign_qa(time_series, ...
+function test_motion(testCase) 
+
+new_files = spmup_realign_qa(input_file(), ...
                              'Motion Parameters', 'on', ...
                              'Framewise displacement', 'off', ...
                              'Globals', 'off', ...
                              'Voltera', 'off', ...
                              'Movie', 'off', ...
-                             'figure', plot_figures);
+                             'figure', plot_figures());
 
 assert(isempty(new_files));
+end
 
-%% FD
-new_files = spmup_realign_qa(time_series, ...
+function test_FD(testCase) 
+
+new_files = spmup_realign_qa(input_file(), ...
                              'Motion Parameters', 'off', ...
                              'Framewise displacement', 'on', ...
                              'Globals', 'off', ...
                              'Voltera', 'off', ...
                              'Movie', 'off', ...
-                             'figure', plot_figures);
+                             'figure', plot_figures());
 
 % 6 motion + FD + RMS + 3 censoring regressors
 motion_and_fd_censor = spm_load(new_files{1});
 assert(size(motion_and_fd_censor, 2) == 11);
 % make sure all censoring regressors are at the end
-assert(all(sum(all_regressors(:, end - 2:end)) == [1 1 1]));
+assert(all(sum(motion_and_fd_censor(:, end - 2:end)) == [1 1 1]));
+end
 
-teardown(new_files);
+function test_volterra(testCase) 
 
-%% Voltera
-new_files = spmup_realign_qa(time_series, ...
+new_files = spmup_realign_qa(input_file(), ...
                              'Motion Parameters', 'off', ...
                              'Framewise displacement', 'off', ...
                              'Globals', 'off', ...
                              'Voltera', 'on', ...
                              'Movie', 'off', ...
-                             'figure', plot_figures);
+                             'figure', plot_figures());
 
 % 6 motion + their derivatives + square of each
 voltera = spm_load(new_files{1});
 assert(size(voltera, 2) == 24);
+end
 
-teardown(new_files);
+function test_globals(testCase) 
 
-%% Globals
-new_files = spmup_realign_qa(time_series, ...
+new_files = spmup_realign_qa(input_file(), ...
                              'Motion Parameters', 'off', ...
                              'Framewise displacement', 'off', ...
                              'Globals', 'on', ...
                              'Voltera', 'off', ...
                              'Movie', 'off', ...
-                             'figure', plot_figures);
+                             'figure', plot_figures());
 
 % 6 motion + one global regressor
 globals = spm_load(new_files{1});
 assert(size(globals, 2) == 7);
+end
 
-teardown(new_files);
+function test_all_together(testCase) 
 
-%% all together
-new_files = spmup_realign_qa(time_series, ...
+new_files = spmup_realign_qa(input_file(), ...
                              'Motion Parameters', 'on', ...
                              'Framewise displacement', 'on', ...
                              'Globals', 'on', ...
                              'Voltera', 'on', ...
                              'Movie', 'off', ...
-                             'figure', plot_figures);
+                             'figure', plot_figures());
 
 % 24 voltera + RMS + FD + global + 3 censoring regressors
 all_regressors = spm_load(new_files{1});
@@ -95,12 +102,11 @@ assert(all(sum(all_regressors(:, end - 2:end)) == [1 1 1]));
 
 metadata = spm_load(spm_file(new_files{1}, 'ext', '.json'));
 assert(strcmp(metadata.Columns{end}, 'outlier_0003'));
+end
 
-teardown(new_files);
-
-%%
-function teardown(new_files)
-    delete(new_files{1});
-    delete(fullfile(test_folder(), 'data', 'sub-01', 'func', '*.ps'));
-    delete(fullfile(test_folder(), 'data', 'sub-01', 'func', '*design.json'));
+%% Optional file fixtures  
+function teardown(testCase)  % do not change function name
+delete(fullfile(test_folder(), 'data', 'sub-01', 'func', '*design.txt'));
+delete(fullfile(test_folder(), 'data', 'sub-01', 'func', '*.ps'));
+delete(fullfile(test_folder(), 'data', 'sub-01', 'func', '*design.json'));
 end
