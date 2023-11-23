502c0fd8487b7f82d18ab3d3a736a964cbe7815c
CPernet <wamcyril@gmail.com>
2023-11-23 11:38:53 +0100

    topup done via SPM

--- utilities/spmup_FSLtopup.m
+++
@@ -1,9 +0,0 @@
-function spmup_FSLtopup
-
-% hack the TOPUP FLS and SPM field map to make it works
-% for Spin Echo magnitude images with inverted phase encoding 
-%
-% Cyril Pernet & Patrick Fisher
-% --------------------------
-%  Copyright (C) SPMUP Team 
-
